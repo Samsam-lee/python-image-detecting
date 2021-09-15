8d4531a2f0eae93201482d7a8084ed425fa8a03e
Seunghyeong Lee <seunghyeong27@gmail.com>
2021-09-15 10:47:24 +0900

    Feature :: ROI 지정 후 원래 이미지에 덮어보여주기

--- TEST/test.py
+++ TEST/test.py
@@ -5,50 +5,46 @@ import matplotlib.pyplot as plt
 import numpy
 
 # gray 이미지 불러오기
-roadImage = cv2.imread('assets/TestTrack1.png', cv2.IMREAD_GRAYSCALE)
-# roadImage = cv2.imread('assets/testImage1.jpeg', cv2.IMREAD_GRAYSCALE)
+# roadImage = cv2.imread('assets/TestTrack1.png', cv2.IMREAD_GRAYSCALE)
+roadImage = cv2.imread('assets/testImage1.jpeg', cv2.IMREAD_GRAYSCALE)
 
 # 가우시안 불러링은 일반적인 불러와 다르게 중심에 있는 픽셀에 높은 가중치 부여
 # 세 번째 아규먼트를 0으로 하면 지정한 커널 크기에 맞추어 시그마를 계산해서 사용
 blurImage = cv2.GaussianBlur(roadImage, (5, 5), 0)
 
 # 테두리 따기
-edgeImage = cv2.Canny(blurImage, 50, 200)
+edgeImage = cv2.Canny(blurImage, 200, 200)
 
-# 관심 영역 설정 (ROI)
+### 관심 영역 설정 (ROI) ###
 
-# height, width = edgeImage.shape[:2]
-# print(height, width)
+# 이미지 크기
+height, width = edgeImage.shape[:2]
+print(height, width)
 
-x=320; y=150; w=100; h=100        # roi 좌표
-roi = edgeImage[y:y+h, x:x+w]         # roi 지정        ---①
+# 흑백 빈 이미지
+mask = numpy.zeros_like(edgeImage)
 
-print(roi.shape)                # roi shape, (50,50)
-cv2.rectangle(roi, (0,0), (h-1, w-1), 255) # roi 전체에 사각형 그리기 ---②
+ignore_mask_color = 255
 
+vertices = numpy.array([[(-(width/4),height), ((width/2)-(width/10),(height/8)), 
+                        ((width/2)+(width/10),(height/8)), (width+(width/4), height)]],dtype=numpy.int32)
+
+# 관심 영역 지정 색으로 표현
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+plt.figure(figsize=(10,8))
+plt.imshow(mask,cmap='gray')
+plt.show()
+
+masked_image = cv2.bitwise_and(edgeImage, mask)
+
+cv2.imshow('masked_image', masked_image)
 
 
 
 
 # 이미지 사용자에게 보여주기
-cv2.imshow("Result Image", edgeImage)
-
-
-
-
-
-# # 다각형 모양 생성
-# vertices = numpy.array([[(50,height),(width/2-45, height/2+60), (width/2+45, height/2+60), (width-50,height)]], dtype=numpy.int32)
-# print(vertices)
-
-# #
-# mask = numpy.zeros_like(edgeImage)
-# cv2.fillPoly(mask, vertices, 255)
-
-# ROI_image = cv2.bitwise_and(edgeImage, mask)
-
-# mark = numpy.copy(ROI_image)
-
+# cv2.imshow("Result Image", edgeImage)
 
 # 동작 하기 전 키 누르는 것 대기
 k = cv2.waitKey(0)
