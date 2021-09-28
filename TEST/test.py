de94342eaa72c6ca420efe795174d306993d27b4
Seunghyeong Lee <seunghyeong27@gmail.com>
2021-09-28 16:42:29 +0900

    Feature :: 배수구 망 제거, 대표선 오류

--- TEST/test.py
+++ TEST/test.py
@@ -8,11 +8,21 @@ import numpy
 # image = cv2.imread('assets/TestTrack1.png')
 # image = cv2.imread('assets/testImage1.jpeg')
 
+def makeCoordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))    
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+
+    return numpy.array([x1, y1, x2, y2])
+
 # 영상 불러오기
-video = cv2.VideoCapture('assets/testVideo1.mp4')
+video = cv2.VideoCapture('assets/testVideoCurve1.mp4')
 
 while(video.isOpened()):
     ret, image = video.read()
+
     try:
         # 이미지 크기
         height, width = image.shape[:2]
@@ -25,8 +35,11 @@ while(video.isOpened()):
         # 세 번째 아규먼트를 0으로 하면 지정한 커널 크기에 맞추어 시그마를 계산해서 사용
         blurImage = cv2.GaussianBlur(roadImage, (5, 5), 0)
 
+        # 배수구 망 제거 (한 번 더 불러링)
+        ret, thrImg = cv2.threshold(blurImage, 170, 255, cv2.THRESH_BINARY)
+
         # 테두리 따기
-        edgeImage = cv2.Canny(blurImage, 200, 200)
+        edgeImage = cv2.Canny(thrImg, 200, 200)
 
         ### 관심 영역 설정 (ROI) ###
         # 흑백 빈 이미지
@@ -46,8 +59,6 @@ while(video.isOpened()):
 
         masked_image = cv2.bitwise_and(edgeImage, mask)
 
-        # cv2.imshow('masked_image', masked_image)
-
         ### Hough Line ###
         rho = 2
         theta = numpy.pi/180
@@ -58,13 +69,36 @@ while(video.isOpened()):
         lines = cv2.HoughLinesP(masked_image, rho, theta, threshold, numpy.array([]), 
                                 minLineLength = min_line_len, maxLineGap = max_line_gap)
 
-        line_image = numpy.zeros((height, width, 3), dtype = numpy.uint8)
+
+        # 대표선
+        leftFit = []
+        rightFit = []
 
         for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(line_image, (x1, y1), (x2, y2), [0, 0, 255], 5)
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = numpy.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                leftFit.append((slope, intercept))
+            else:
+                rightFit.append((slope, intercept))
+        
+        leftFitAverage = numpy.average(leftFit, axis=0)
+        rightFitAverage = numpy.average(rightFit, axis=0)
+        leftLine = makeCoordinates(image, leftFitAverage)
+        rightLine = makeCoordinates(image, rightFitAverage)
 
-        # cv2.imshow('line_image', line_image)
+        averageLine = numpy.array([leftLine, rightLine])
+
+        #########################################################대표선 오류############
+
+        # line_image = numpy.zeros((height, width, 3), dtype = numpy.uint8)
+        line_image = numpy.zeros_like(image)
+        if averageLine is not None:
+            for line in averageLine:
+                for x1, y1, x2, y2 in line:
+                    cv2.line(line_image, (x1, y1), (x2, y2), [0, 0, 255], 5)
 
     except:
         continue
@@ -86,6 +120,9 @@ k = cv2.waitKey(0)
 # 27 -> esc
 if k == 27:
     cv2.destroyAllWindows();
+
+
+
 
 # # ord('s') -> s키
 # elif k == ord('s'):
